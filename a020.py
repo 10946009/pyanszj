bba18bc1077b850488762766d1b3470bcb566783
10946009 <108606810+10946009@users.noreply.github.com>
2023-02-12 10:33:08 +0800

    fix a020

--- a020.py
+++ a020.py
@@ -1,4 +1,28 @@
-Dic = {"A":10, "B":11, "C":12, "D":13, "E":14}
-info = input()
-ajs = Dic[2]
-print(ajs[1])
+a=list(input())
+first_eng=ord(a[0])-55
+if first_eng ==18:
+  first_eng=34
+elif first_eng==24:
+  first_eng=35
+elif first_eng==33:
+  first_eng=30
+elif first_eng==32:
+  first_eng=32
+elif first_eng==34:
+  first_eng=31
+elif first_eng==35:
+  first_eng=33
+elif first_eng>24:
+  first_eng-=2
+elif first_eng>18:
+  first_eng-=1
+j=8
+a020=0
+for i in range(1,9):
+  a020+=int(a[i])*j
+  j-=1
+a020+=(first_eng//10)+(first_eng%10)*9+int(a[9])
+if a020%10==0:
+  print("real")
+else:
+  print("fake")
