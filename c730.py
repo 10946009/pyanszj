063e7d51f7f4ac27a20505b05f2d020655dc8c8e
10946009 <108606810+10946009@users.noreply.github.com>
2023-02-12 10:29:15 +0800

    Delete c730.py

--- c730.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-for temp in sys.stdin:
-    data = [int(e) for e in temp.split()]
-    threshold = int(input())
-    data = [e for e in data if e <= threshold + 30]
-    print(len(data))
