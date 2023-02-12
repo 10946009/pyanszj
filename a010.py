5473d485d754cfdf1dbd33d5af3dbc35ba4c815b
10946009 <108606810+10946009@users.noreply.github.com>
2023-02-12 10:31:56 +0800

    fix

--- a010.py
+++ a010.py
@@ -1,16 +1,20 @@
-num = int(input())
-ans = ""
-for i in range(2, num+1):
-    flag = 0
-    while num % i == 0:
-        num = num / i
-        flag += 1
-    if flag > 1:
-        ans += str(i) + "^" + str(flag)
-        if num > 1: 
-            ans += " * "
-    elif flag == 1:
-        ans += str(i)
-        if num > 1:
-            ans += " * "
-print(ans)
+a = int(input())
+b=2
+if a==1:
+  print(1)
+elif a>=2:
+  while a>=b:
+    c=1
+    if a%b==0 and a!=1:
+      print(b,end='')
+      a/=b
+      if a%b==0 and a!=1:
+        while(a%b==0 and a!=1):
+          c+=1
+          a/=b
+        print(f"^{c}",end='')
+      if a!=1:
+        print(" * ",end='')
+    b+=1
+else:
+  print(0)
